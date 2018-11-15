1a151c6df6f873f47306620e204352e2336fe44a
Ethan Rosenberg <ethan@ethanrosenberg.com>
2018-11-15 06:48:04 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,7 +1,7 @@
 def oxford_comma(array)
   if array.size == 1
     return array[0]
-  else if array.size == 2
+  elsif array.size == 2
     return array[0] + "and" + array[1]
   else
     initial = array[0..array.length - 2].join(", ")
